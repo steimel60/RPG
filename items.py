5f94381840fce33c1323a31ac3982a7cb65758e8
Dylan Steimel <dylansteimel60@gmail.com>
2021-10-01 14:56:54 -0400

    Equippables and armor

--- items.py
+++ items.py
@@ -11,8 +11,23 @@ class Item():
         for detail in details:
             print(f'{detail}: {details[detail]}')
 
-class Wand(Item):
+    def is_equippable(self):
+        return isinstance(self,Equippable)
+
+    def is_armor(self):
+        return isinstance(self,Armor)
+
+class Equippable(Item):
+    def __init__(self):
+        super().__init__()
+
+class Armor(Equippable):
+    def __init__(self):
+        super().__init__()
+
+class Wand(Equippable):
     def __init__(self, wood, core, length, flex, maker=None):
+        super().__init__()
         #Wand Details
         self.wood = wood
         self.core = core
@@ -21,7 +36,6 @@ class Wand(Item):
         self.type = 'Wand'
         self.maker = maker
         self.name = f'{self.wood} {self.type}'
-
 
     def change_wood(self):
         self.wood = 'Elder'
@@ -37,10 +51,41 @@ class Wand(Item):
         }
         return details
 
+class Cloak(Armor):
+    def __init__(self):
+        super().__init__()
+    def get_details(self):
+        return {}
+
+class Cauldron(Item):
+    def __init__(self, maker=None):
+        super().__init__()
+    def get_details(self):
+        return {}
+
 wand = Wand('Larch', 'Dragon Heartstring', '11 inches', 'Swishy')
+cauldron = Cauldron()
+cloak = Cloak()
+print('Wand Print Statements')
 wand.print_details()
-print(wand.name)
-wand.change_wood()
+print(f'Item Instance: {isinstance(wand,Item)}')
+print(f'Equippable Instance: {isinstance(wand,Equippable)}')
+print(f'Armor Instance: {isinstance(wand,Armor)}')
+print(f'Wand Instance: {isinstance(wand,Wand)}')
+print(f'Cauldron Instance: {isinstance(wand,Cauldron)}')
 print('\n')
-wand.print_details()
-print(wand.name)
+print('Cauldron Print Statements')
+print(f'Item Instance: {isinstance(cauldron,Item)}')
+print(f'Equippable Instance: {isinstance(cauldron,Equippable)}')
+print(f'Armor Instance: {isinstance(cauldron,Armor)}')
+print(f'Wand Instance: {isinstance(cauldron,Wand)}')
+print(f'Cauldron Instance: {isinstance(cauldron,Cauldron)}')
+cauldron.print_details()
+print('\n')
+print('Cloak Print Statements')
+print(f'Item Instance: {isinstance(cloak,Item)}')
+print(f'Equippable Instance: {isinstance(cloak,Equippable)}')
+print(f'Armor Instance: {isinstance(cloak,Armor)}')
+print(f'Wand Instance: {isinstance(cloak,Wand)}')
+print(f'Cauldron Instance: {isinstance(cloak,Cauldron)}')
+cloak.print_details()
